c0e55e243f52a89379bf4007ff99f52bcc12e2ec
chayanbajaj <chayanbajaj@gmail.com>
2022-11-18 13:10:36 +0530

    checked working

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+
+//routes
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App')
+})
+const port = 5000;
+
+app.listen(port, () => `listening on server ${port}...`)
