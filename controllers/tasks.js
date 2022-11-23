a5c7acdded09c230e5af90889b6a4bfc52a8eb6d
chayanbajaj <chayanbajaj@gmail.com>
2022-11-23 17:32:01 +0530

    added method for creating task in controllers using the schema

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,3 +1,5 @@
+const Task = require('../models/Task')
+
 const getAllTasks = (req, res) => {
      res.send('get all tasks')
 }
@@ -6,8 +8,9 @@ const getTask = (req, res) => {
     res.send('get single task')
 }
 
-const createTask = (req, res) => {
-    res.send('create task')
+const createTask = async (req, res) => {
+    const task = await Task.create(req.body)
+    res.status(201).json({ task })
 }
 
 const updateTask = (req, res) => {
